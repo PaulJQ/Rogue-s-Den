17dd62c2d78c4dce1d9649864032115131f76dc7
Paul <pauljq48@gmail.com>
2019-10-04 21:42:37 -0400

    first commit

--- Assets/Recources/My Scripts/PlayerController.cs
+++ Assets/Recources/My Scripts/PlayerController.cs
@@ -12,7 +12,6 @@ public class PlayerController : MonoBehaviour
 	public float speed;
 	private Rigidbody2D rb2d;
 	public float jumpHeight;
-	public SpriteRenderer spriteRenderer;
 	private static int count = 0;
 
 
@@ -20,7 +19,6 @@ public class PlayerController : MonoBehaviour
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
-		SpriteRenderer = GetComponent<SpriteRenderer>();
 		Scene currentScene = SceneManager.GetActiveScene();
 		
 		if (!currentScene.isLoaded){
@@ -34,7 +32,7 @@ public class PlayerController : MonoBehaviour
 		Console.Out.WriteLine(count);
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		//Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		Vector2 movement = new Vector2 (moveHorizontal, 0);
+		Vector2 movement = new Vector2 ((moveHorizontal!=0?1:0) * Mathf.Sign(moveHorizontal), 0);
 		//Vector2 Nmovement = new Vector2 (moveHorizontal * -1, 0);
 		Vector2 jump = new Vector2 (0f, 2f);
 		if (Input.GetKeyDown (KeyCode.W)) {
@@ -43,19 +41,27 @@ public class PlayerController : MonoBehaviour
 		}
 
 		rb2d.AddForce (movement * speed);
+
+        if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.D)){
+            Vector2 velocity = rb2d.velocity;
+            velocity.x = 0;
+            rb2d.velocity = velocity;
+        }
+
 		//controller.Move(moveHorizontal * Time.deltaTime, false, jump);
 		
-        if (Input.GetKeyDown (KeyCode.A)) {
-			rb2d.AddForce (movement * speed);
+        /*if (Input.GetKey (KeyCode.A)) {
+			rb2d.AddForce (movement * speed, ForceMode2D.Impulse);
 		}
 			
-		if (Input.GetKeyDown (KeyCode.D)) {
-			rb2d.AddForce (movement * -speed);
+		if (Input.GetKey (KeyCode.D)) {
+			rb2d.AddForce (movement * -speed, ForceMode2D.Impulse);
 		}
 
 		if (Input.GetKeyDown (KeyCode.J)) {
 			
 		}
+		*/
 
 		}
 	}
