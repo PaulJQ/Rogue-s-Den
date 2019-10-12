529f4cd92e31b8e17852260cb6c2e608b8f03f53
Paul <pauljq48@gmail.com>
2019-10-11 22:06:34 -0400

    finished parallax backgrounds

---
+++ PlayerController2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController2 : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+}
